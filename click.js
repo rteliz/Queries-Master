68dbcf2fe6d65f764467b747ad93f46e388a4730
rteliz <9rteliz@stpatrick.org>
2017-03-24 13:54:46 -0500

    3/24/17

---
+++ click.js
@@ -0,0 +1,17 @@
+//declare samecolor() here
+function samecolor(){
+  let doc = document ;
+  let color = #"009900";
+
+  let divs = doc.querySelectorALL("#samecolor div");
+
+  for(var i=0, , i++){
+    divs[0].style.backgroundColor=color;
+    divs[0].style.height = "500px"
+    divs[1].style.opacity = 0.75
+  }
+}
+//declare diffcolor() here
+function diffcolor(){
+  let color = "#9933s00"
+}
